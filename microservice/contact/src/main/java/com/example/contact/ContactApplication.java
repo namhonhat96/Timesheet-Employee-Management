b3c6feb9b35abd78156d02bb0dba46fcb14caaba
Ian Chai <xchai0711@gmail.com>
2020-12-14 19:15:43 -0500

    backend routing integration

--- microservice/contact/src/main/java/com/example/contact/ContactApplication.java
+++ microservice/contact/src/main/java/com/example/contact/ContactApplication.java
@@ -2,8 +2,10 @@ package com.example.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContactApplication {
 
 	public static void main(String[] args) {
