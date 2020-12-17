c9fac57077bc460121916a4c3769b4178dfde066
Nam Ho <namhonhat96@gmail.com>
2020-12-17 11:30:05 -0500

    solve merge conflict

--- react/front-end/src/Summary.js
+++ react/front-end/src/Summary.js
@@ -4,37 +4,19 @@ import axios from "axios";
 export default class Summary extends React.Component {
   //Define the variable names from backend pojo
 
-  //create a table to display result 
+  //create a table to display result
 
   //componentdidmount
   constructor(props) {
-    super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-    this.state = { //state is by default an object
-       summarys: [],
-  
-       count: 5
-    }
- }
-//  state = { WeekEnding: "", hours: "", submissionStatus: "", approvalStatus:"", option:"", comment:"" };
+    super(props); //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = {
+      //state is by default an object
+      summarys: [],
 
- renderTableData() {
-      return this.state.summarys.map((student, index) => {
-         const { id, weekEnding, hours, submissionStatus, approvalStatus,option,  comment} = student
-         return (
-            <tr key={id}>
-               <td>{weekEnding}</td>
-               <td>{hours}</td>
-               <td title='hr....'>{submissionStatus}</td>
-               <td>{approvalStatus}</td>
-               <td onClick={this.handleOption()}>{option}</td>
-               <td>{comment}</td>
-
-<<<<<<< HEAD
-            </tr>
-         )
-      })
-   }
-=======
+      count: 5,
+    };
+  }
+  //  state = { WeekEnding: "", hours: "", submissionStatus: "", approvalStatus:"", option:"", comment:"" };
   renderTableData() {
     return this.state.summarys.map((student, index) => {
       const {
@@ -58,59 +40,51 @@ export default class Summary extends React.Component {
       );
     });
   }
->>>>>>> main
-
-handleOption(){ }
+  handleOption() {}
 
   componentDidMount() {
- 
     let userId = localStorage.getItem("userID");
     this.userId = "1";
-    console.log(this.userId+"-------")
-    axios.get(`http://localhost:8084/timesheet/summary?userId=`+this.userId).then((res) => {
-      this.setState ({summarys : res.data});
-    });
-    
+    console.log(this.userId + "-------");
+    axios
+      .get(`http://localhost:8084/timesheet/summary?userId=` + this.userId)
+      .then((res) => {
+        this.setState({ summarys: res.data });
+      });
   }
 
   handleShowMore() {
-
-    this.setState ({
-      count: this.state.summarys.length
+    this.setState({
+      count: this.state.summarys.length,
     });
   }
-
 
   render() {
     const count = this.count;
     return (
       <div>
-      <table>
-        <tr>
-          <th>WeekEnding</th>
-          <th>Total Hours</th>
-          <th>Submission Status</th>
-          <th>Approval Status</th>
-          <th>Option</th>
-          <th>Comment</th>
-        </tr>
-        <tr></tr>
-        <tbody>
-          {this.renderTableData()}
-        </tbody>
-      </table>
-      
-      <button
-      type="button"
-      className="button-time"
-      button-time
-      onClick={this.handleShowMore}
-    >
-      Show More
-    </button>
-    </div>
+        <table>
+          <tr>
+            <th>WeekEnding</th>
+            <th>Total Hours</th>
+            <th>Submission Status</th>
+            <th>Approval Status</th>
+            <th>Option</th>
+            <th>Comment</th>
+          </tr>
+          <tr></tr>
+          <tbody>{this.renderTableData()}</tbody>
+        </table>
 
-
+        <button
+          type="button"
+          className="button-time"
+          button-time
+          onClick={this.handleShowMore}
+        >
+          Show More
+        </button>
+      </div>
     );
   }
 }
