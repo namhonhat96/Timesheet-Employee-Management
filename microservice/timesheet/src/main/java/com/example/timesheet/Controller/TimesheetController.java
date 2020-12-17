9d09db170d0585046724d1ecffd84b4df6856174
hlys57 <hlysh57@hotmail.com>
2020-12-16 20:31:47 -0500

    conflict

--- microservice/timesheet/src/main/java/com/example/timesheet/Controller/TimesheetController.java
+++ microservice/timesheet/src/main/java/com/example/timesheet/Controller/TimesheetController.java
@@ -1,7 +1,7 @@
 package com.example.timesheet.Controller;
 
 import com.example.timesheet.Domain.Timesheet;
-import com.example.timesheet.Repository.TimesheetRepository;
+import com.example.timesheet.repository.TimesheetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +24,7 @@ public class TimesheetController {
 
     // list the summary the week
     @GetMapping("/summary")
-    public List<Timesheet> getListOfTimesheet(HttpServletResponse response,  @RequestParam String userId) {
+    public List<Timesheet> getListOfTimesheet(HttpServletResponse response,  @RequestParam Integer userId) {
         response.setHeader("Access-Control-Allow-Origin", "*");
 
         List<Timesheet> list = timesheetRepo.findAllByUserId(userId);
@@ -37,14 +37,14 @@ public class TimesheetController {
     }
 
     @GetMapping("/week")
-    public Timesheet getOneTimesheet(@RequestParam String userId, @RequestParam String weekEnding) {
+    public Timesheet getOneTimesheet(@RequestParam Integer userId, @RequestParam String weekEnding) {
         return timesheetRepo.findByUserIdAndWeekEnding(userId, weekEnding);
     }
 
     @PostMapping("/add")
     public ResponseEntity<String> addTimesheet() {
          Timesheet timesheet = new Timesheet();
-         timesheet.setUserId("123");
+         timesheet.setUserId(123);
          timesheet.setSubmissionStatus(2);
          timesheet.setApprovalStatus(1);
         timesheetRepo.save(timesheet);
